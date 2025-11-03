8e5098e8a5edd6ac180e3479caf526a92d684b35
manoel-lopes <manoel.lopes.shell@gmail.com>
2025-11-03 11:06:16 -0300

    feat: add prisma service and client configuration

---
+++ src/infra/persistence/prisma/client.ts
@@ -0,0 +1,12 @@
+import { env } from '@/lib/env'
+import { PrismaClient } from '@prisma/client'
+
+const log: Record<string, ('query' | 'info' | 'warn' | 'error')[]> = {
+  development: ['query'],
+  production: ['error', 'warn'],
+  test: [],
+}
+
+export const prisma = new PrismaClient({
+  log: log[env.NODE_ENV || 'development'],
+})
