67798748599b8dfefc44138c4881e946d6dc6aae
manoel-lopes <manoel.lopes.shell@gmail.com>
2025-11-03 11:07:03 -0300

    style: adjust main application bootstrap formatting

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
+import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { env } from './lib/env'
-import { NestFactory } from '@nestjs/core'
 
 async function bootstrap () {
   const app = await NestFactory.create(AppModule)
