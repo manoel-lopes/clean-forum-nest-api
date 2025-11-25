d14fe725d0bef921a91cb685b8534b74e3b23289
manoel-lopes <manoel.lopes.shell@gmail.com>
2025-11-25 13:56:55 -0300

    feat(infra): add test doubles for use cases

---
+++ src/infra/doubles/use-case.stub.ts
@@ -0,0 +1,7 @@
+import { UseCase } from '@/core/domain/application/use-case'
+
+export class UseCaseStub implements UseCase {
+  async execute (_input?: unknown): Promise<unknown> {
+    return null
+  }
+}
