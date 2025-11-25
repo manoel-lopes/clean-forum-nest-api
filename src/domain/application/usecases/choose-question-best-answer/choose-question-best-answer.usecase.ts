4df63dd62fe56b2ea6fdc0c0ff35c5816eface72
manoel-lopes <manoel.lopes.shell@gmail.com>
2025-11-25 13:52:57 -0300

    refactor(usecases): update choose best answer use case and tests

--- src/domain/application/usecases/choose-question-best-answer/choose-question-best-answer.usecase.ts
+++ src/domain/application/usecases/choose-question-best-answer/choose-question-best-answer.usecase.ts
@@ -1,6 +1,7 @@
-import type { UseCase } from '@/core/domain/application/use-case'
-import type { AnswersRepository } from '@/domain/application/repositories/answers.repository'
-import type { QuestionsRepository } from '@/domain/application/repositories/questions.repository'
+import { Inject, Injectable } from '@nestjs/common'
+import { UseCase } from '@/core/domain/application/use-case'
+import { AnswersRepository } from '@/domain/application/repositories/answers.repository'
+import { QuestionsRepository } from '@/domain/application/repositories/questions.repository'
 import type { Question } from '@/domain/enterprise/entities/question.entity'
 import { NotAuthorError } from '@/shared/application/errors/not-author.error'
 import { ResourceNotFoundError } from '@/shared/application/errors/resource-not-found.error'
@@ -12,10 +13,11 @@ type ChooseQuestionBestAnswerRequest = {
 
 export type ChooseQuestionBestAnswerResponse = Omit<Question, 'answers'>
 
+@Injectable()
 export class ChooseQuestionBestAnswerUseCase implements UseCase {
   constructor (
-    private readonly questionsRepository: QuestionsRepository,
-    private readonly answersRepository: AnswersRepository
+    @Inject(QuestionsRepository) private readonly questionsRepository: QuestionsRepository,
+    @Inject(AnswersRepository) private readonly answersRepository: AnswersRepository
   ) {
     Object.freeze(this)
   }
