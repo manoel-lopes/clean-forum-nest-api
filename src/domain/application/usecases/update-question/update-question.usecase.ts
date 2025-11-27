d034cd9d70ac9bc439b4d8ce0e0d633837664af6
manoel-lopes <manoel.lopes.shell@gmail.com>
2025-11-27 15:58:25 -0300

    fix(update-question): add author validation to prevent unauthorized updates
    Adds authorization check to verify the requesting user is the author
    of the question before allowing updates. Throws NotAuthorError if the
    user is not the owner.

--- src/domain/application/usecases/update-question/update-question.usecase.ts
+++ src/domain/application/usecases/update-question/update-question.usecase.ts
@@ -2,10 +2,12 @@ import { Inject, Injectable } from '@nestjs/common'
 import { UseCase } from '@/core/domain/application/use-case'
 import { QuestionsRepository, type UpdateQuestionData } from '@/domain/application/repositories/questions.repository'
 import type { Question } from '@/domain/enterprise/entities/question.entity'
+import { NotAuthorError } from '@/shared/application/errors/not-author.error'
 import { ResourceNotFoundError } from '@/shared/application/errors/resource-not-found.error'
 
 type UpdateQuestionRequest = UpdateQuestionData['data'] & {
   questionId: string
+  authorId: string
 }
 
 @Injectable()
@@ -17,10 +19,13 @@ export class UpdateQuestionUseCase implements UseCase {
   }
 
   async execute (req: UpdateQuestionRequest): Promise<Question> {
-    const { questionId, title, content } = req
+    const { questionId, authorId, title, content } = req
     const question = await this.questionsRepository.findById(questionId)
     if (!question) {
       throw new ResourceNotFoundError('Question')
+    }
+    if (question.authorId !== authorId) {
+      throw new NotAuthorError('question')
     }
     const updatedQuestion = await this.questionsRepository.update({
       where: { id: questionId },
