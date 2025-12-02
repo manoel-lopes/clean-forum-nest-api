c8f99755ca30b3f0374dae0a606faa100f25d209
manoel-lopes <manoel.lopes.shell@gmail.com>
2025-12-02 10:00:52 -0300

    refactor(tests): update Slug import path in make-question factory

--- tests/factories/domain/make-question.ts
+++ tests/factories/domain/make-question.ts
@@ -1,5 +1,5 @@
 import type { Question, QuestionProps } from '@/domain/enterprise/entities/question.entity'
-import { Slug } from '@/domain/enterprise/entities/value-objects/slug/slug.vo'
+import { Slug } from '@/domain/enterprise/value-objects/slug/slug.vo'
 import { faker } from '@faker-js/faker'
 
 export function makeQuestionData (override: Partial<Question> = {}): QuestionProps {
