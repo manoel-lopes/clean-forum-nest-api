16dee5e57fbe7be7964c03a2b3ad49d74fc184a4
manoel-lopes <manoel.lopes.shell@gmail.com>
2025-12-02 09:51:32 -0300

    refactor: remove deprecated shared error files
    Remove NotAuthorError and ResourceNotFoundError in favor
    of the new *Exception classes.

--- src/shared/application/errors/resource-not-found.error.ts
+++
@@ -1,7 +0,0 @@
-type Resource = 'User' | 'Answer' | 'Question' | 'Comment' | 'Refresh token' | 'Email validation' | 'Attachment'
-
-export class ResourceNotFoundError extends Error {
-  constructor (resource: Resource) {
-    super(`${resource} not found`)
-  }
-}
