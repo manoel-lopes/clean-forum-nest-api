cd7c1d34862ece35404c394f0919d588240b1f1b
manoel-lopes <manoel.lopes.shell@gmail.com>
2025-12-06 16:09:22 -0300

    docs(swagger): add ApiOperation summary to update-question-comment controller

--- src/presentation/controllers/update-question-comment/update-question-comment.controller.ts
+++ src/presentation/controllers/update-question-comment/update-question-comment.controller.ts
@@ -1,6 +1,14 @@
-import { Controller } from '@nestjs/common'
-import { ApiTags } from '@nestjs/swagger'
+import { Controller, Put } from '@nestjs/common'
+import { ApiOperation, ApiTags } from '@nestjs/swagger'
 import { UpdateCommentUseCase } from '@/domain/application/usecases/update-comment/update-comment.usecase'
+import {
+  ApiForbiddenResponse,
+  ApiInternalServerErrorResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiUnauthorizedResponse,
+  ApiUnprocessableEntityResponse,
+} from '@/presentation/decorators/api-responses.decorator'
 import { BaseUpdateCommentController } from '../base/base-update-comment.controller'
 
 @ApiTags('Comments')
@@ -8,5 +16,17 @@ import { BaseUpdateCommentController } from '../base/base-update-comment.control
 export class UpdateQuestionCommentController extends BaseUpdateCommentController {
   constructor (updateCommentUseCase: UpdateCommentUseCase) {
     super(updateCommentUseCase)
+  }
+
+  @Put()
+  @ApiOperation({ summary: 'Update a question comment' })
+  @ApiOkResponse('Comment updated successfully')
+  @ApiUnauthorizedResponse()
+  @ApiForbiddenResponse()
+  @ApiNotFoundResponse('Comment not found')
+  @ApiUnprocessableEntityResponse()
+  @ApiInternalServerErrorResponse()
+  override handle (...args: Parameters<BaseUpdateCommentController['handle']>) {
+    return super.handle(...args)
   }
 }
