d6712c49e8768f477a0e3b2c78cfe3addceb341b
manoel-lopes <manoel.lopes.shell@gmail.com>
2025-12-06 16:48:03 -0300

    docs(swagger): add ApiOperation summary to delete-question-comment controller

--- src/presentation/controllers/delete-question-comment/delete-question-comment.controller.ts
+++ src/presentation/controllers/delete-question-comment/delete-question-comment.controller.ts
@@ -1,6 +1,14 @@
-import { Controller } from '@nestjs/common'
-import { ApiTags } from '@nestjs/swagger'
+import { Controller, Delete, HttpCode } from '@nestjs/common'
+import { ApiOperation, ApiTags } from '@nestjs/swagger'
 import { DeleteQuestionCommentUseCase } from '@/domain/application/usecases/delete-question-comment/delete-question-comment.usecase'
+import {
+  ApiForbiddenResponse,
+  ApiInternalServerErrorResponse,
+  ApiNoContentResponse,
+  ApiNotFoundResponse,
+  ApiUnauthorizedResponse,
+  ApiUnprocessableEntityResponse,
+} from '@/presentation/decorators/api-responses.decorator'
 import { BaseDeleteCommentController } from '../base/base-delete-comment.controller'
 
 @ApiTags('Comments')
@@ -8,5 +16,18 @@ import { BaseDeleteCommentController } from '../base/base-delete-comment.control
 export class DeleteQuestionCommentController extends BaseDeleteCommentController {
   constructor (deleteQuestionCommentUseCase: DeleteQuestionCommentUseCase) {
     super(deleteQuestionCommentUseCase)
+  }
+
+  @Delete()
+  @HttpCode(204)
+  @ApiOperation({ summary: 'Delete a question comment' })
+  @ApiNoContentResponse('Comment deleted successfully')
+  @ApiUnauthorizedResponse()
+  @ApiForbiddenResponse()
+  @ApiNotFoundResponse('Comment not found')
+  @ApiUnprocessableEntityResponse()
+  @ApiInternalServerErrorResponse()
+  override handle (...args: Parameters<BaseDeleteCommentController['handle']>) {
+    return super.handle(...args)
   }
 }
