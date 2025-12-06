61d668b4d31dc84521191b8448453c29fac06ede
manoel-lopes <manoel.lopes.shell@gmail.com>
2025-12-06 15:06:41 -0300

    refactor(shared): remove unused log-level.ts file

--- src/shared/types/log-level.ts
+++
@@ -1,7 +0,0 @@
-export type LogLevel = 'silent' | 'info' | 'error'
-
-export type Environment = 'test' | 'development' | 'production'
-
-export type LogLevelConfig = {
-  [K in Environment]: LogLevel
-}
