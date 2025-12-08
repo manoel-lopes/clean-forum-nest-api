cccd713e0b13843aa4e66d9d6d7515e7bff32ccb
manoel-lopes <manoel.lopes.shell@gmail.com>
2025-12-08 14:44:39 -0300

    feat: add update answer comment use case

---
+++ src/domain/application/usecases/update-answer-comment/update-answer-comment.usecase.test.ts
@@ -0,0 +1,53 @@
+import { InMemoryAnswerCommentsRepository } from '@/infra/persistence/repositories/in-memory/in-memory-answer-comments.repository'
+import { UpdateAnswerCommentUseCase } from './update-answer-comment.usecase'
+import { makeAnswerCommentData } from '@tests/factories/domain/make-answer-comment'
+
+describe('UpdateAnswerCommentUseCase', () => {
+  let sut: UpdateAnswerCommentUseCase
+  let answerCommentsRepository: InMemoryAnswerCommentsRepository
+
+  beforeEach(() => {
+    answerCommentsRepository = new InMemoryAnswerCommentsRepository()
+    sut = new UpdateAnswerCommentUseCase(answerCommentsRepository)
+  })
+
+  it('should not update a nonexistent comment', async () => {
+    const request = {
+      commentId: 'nonexistent-comment-id',
+      authorId: 'author-id',
+      content: 'Updated content',
+    }
+
+    await expect(sut.execute(request)).rejects.toThrow('Comment not found')
+  })
+
+  it('should not update a comment if the user is not the author', async () => {
+    const comment = await answerCommentsRepository.create(makeAnswerCommentData({ authorId: 'comment-author-id' }))
+
+    const request = {
+      commentId: comment.id,
+      authorId: 'unauthorized-user-id',
+      content: 'Updated content',
+    }
+
+    await expect(sut.execute(request)).rejects.toThrow('The user is not the author of the comment')
+  })
+
+  it('should update an answer comment', async () => {
+    const comment = await answerCommentsRepository.create(makeAnswerCommentData({
+      authorId: 'comment-author-id',
+      content: 'Original content',
+    }))
+
+    const request = {
+      commentId: comment.id,
+      authorId: comment.authorId,
+      content: 'Updated content',
+    }
+
+    const response = await sut.execute(request)
+
+    expect(response.id).toBe(comment.id)
+    expect(response.content).toBe('Updated content')
+  })
+})
---
+++ src/domain/application/usecases/update-answer-comment/update-answer-comment.usecase.ts
@@ -0,0 +1,13 @@
+import { Inject, Injectable } from '@nestjs/common'
+import { UseCase } from '@/core/domain/application/use-case'
+import { AnswerCommentsRepository } from '@/domain/application/repositories/answer-comments.repository'
+import { BaseUpdateCommentUseCase } from '../base/base-update-comment-use-case'
+
+@Injectable()
+export class UpdateAnswerCommentUseCase extends BaseUpdateCommentUseCase implements UseCase {
+  constructor (
+    @Inject(AnswerCommentsRepository) answerCommentsRepository: AnswerCommentsRepository
+  ) {
+    super(answerCommentsRepository)
+  }
+}
