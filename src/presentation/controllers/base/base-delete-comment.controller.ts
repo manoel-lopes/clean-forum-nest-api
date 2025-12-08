d445d280ad33d04de5246330eae9b94c9e06bd08
manoel-lopes <manoel.lopes.shell@gmail.com>
2025-12-08 14:45:14 -0300

    refactor: move swagger decorators to base delete comment controller

--- src/presentation/controllers/base/base-delete-comment.controller.ts
+++ src/presentation/controllers/base/base-delete-comment.controller.ts
@@ -5,6 +5,7 @@ import {
   NotFoundException,
   Param,
 } from '@nestjs/common'
+import { ApiOperation } from '@nestjs/swagger'
 import { UseCase } from '@/core/domain/application/use-case'
 import { CurrentUser } from '@/infra/auth/decorators/current-user.decorator'
 import type { AuthUser } from '@/infra/auth/strategies/jwt.strategy'
@@ -13,16 +14,31 @@ import {
   type DeleteCommentParams,
   deleteCommentParamsSchema,
 } from '@/infra/validation/schemas/presentation/comments/delete-comment.schema'
+import {
+  ApiForbiddenResponse,
+  ApiInternalServerErrorResponse,
+  ApiNoContentResponse,
+  ApiNotFoundResponse,
+  ApiUnauthorizedResponse,
+  ApiUnprocessableEntityResponse,
+} from '@/presentation/decorators/api-responses.decorator'
 import { NotAuthorException } from '@/shared/application/exceptions/not-author.exception'
 import { ResourceNotFoundException } from '@/shared/application/exceptions/resource-not-found.exception'
 
 export abstract class BaseDeleteCommentController {
   constructor (
-    protected readonly deleteCommentUseCase: UseCase
+    private readonly deleteCommentUseCase: UseCase
   ) {}
 
   @Delete()
   @HttpCode(204)
+  @ApiOperation({ summary: 'Delete an comment' })
+  @ApiNoContentResponse('Comment deleted successfully')
+  @ApiUnauthorizedResponse()
+  @ApiForbiddenResponse()
+  @ApiNotFoundResponse('Comment not found')
+  @ApiUnprocessableEntityResponse()
+  @ApiInternalServerErrorResponse()
   async handle (
     @CurrentUser() user: AuthUser,
     @Param(new ZodValidationPipe(deleteCommentParamsSchema)) params: DeleteCommentParams
