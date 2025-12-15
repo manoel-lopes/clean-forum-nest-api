da85a1de47d99aeb1fc5d8d39c0691808364c813
manoel-lopes <manoel.lopes.shell@gmail.com>
2025-12-14 21:40:27 -0300

    feat: add upload attachment use case

---
+++ src/domain/application/usecases/upload-attachment/upload-attachment.usecase.ts
@@ -0,0 +1,39 @@
+import { Inject, Injectable } from '@nestjs/common'
+import { UseCase } from '@/core/domain/application/use-case'
+import { StorageService } from '@/infra/adapters/storage/ports/storage-service'
+import { InvalidAttachmentTypeException } from './exceptions/invalid-attachment-type.exception'
+
+type UploadAttachmentRequest = {
+  fileName: string
+  fileType: string
+  body: Buffer
+}
+
+type UploadAttachmentResponse = {
+  url: string
+  key: string
+}
+
+const ALLOWED_FILE_TYPES = [
+  'image/jpeg',
+  'image/png',
+  'image/gif',
+  'image/webp',
+  'application/pdf',
+]
+
+@Injectable()
+export class UploadAttachmentUseCase implements UseCase {
+  constructor (
+    @Inject(StorageService) private readonly storageService: StorageService
+  ) {}
+
+  async execute (request: UploadAttachmentRequest): Promise<UploadAttachmentResponse> {
+    const { fileName, fileType, body } = request
+    if (!ALLOWED_FILE_TYPES.includes(fileType)) {
+      throw new InvalidAttachmentTypeException(fileType)
+    }
+    const { url, key } = await this.storageService.upload({ fileName, fileType, body })
+    return { url, key }
+  }
+}
