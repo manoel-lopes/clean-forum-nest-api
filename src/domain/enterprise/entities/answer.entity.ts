32009c6752999f6a052f0debf1be6244098558f1
manoel-lopes <manoel.lopes.shell@gmail.com>
2025-12-20 16:41:14 -0300

    refactor(entities): convert answer to typeorm entity class

--- src/domain/enterprise/entities/answer.entity.ts
+++ src/domain/enterprise/entities/answer.entity.ts
@@ -1,11 +1,53 @@
-import type { Entity } from '@/core/domain/entity'
-import type { Props } from '@/shared/types/custom/props'
+import { Column, Entity, Index, JoinColumn, ManyToOne, OneToMany } from 'typeorm'
+import { Optional } from '@/shared/types/common/optional'
+import { Props } from '@/shared/types/custom/props'
+import { Attachment } from './base/attachment.entity'
+import { BaseEntity } from './base/base.entity'
+import { Comment } from './base/comment.entity'
+import { Question } from './question.entity'
+import { User } from './user.entity'
 
-export type AnswerProps = Props<Answer>
+export type AnswerProps = Optional<Props<Answer>, 'excerpt'>
 
-export interface Answer extends Entity {
-  content: string
-  questionId: string
-  authorId: string
-  excerpt: string
+@Entity('answers')
+@Index('answers_questionId_idx', ['questionId'])
+@Index('answers_authorId_idx', ['authorId'])
+@Index('answers_createdAt_idx', ['createdAt'])
+@Index('answers_questionId_createdAt_idx', ['questionId', 'createdAt'])
+export class Answer extends BaseEntity {
+  @Column({ type: 'text' })
+  readonly content: string
+
+  @Column({ type: 'varchar', length: 36 })
+  readonly authorId: string
+
+  @Column({ type: 'varchar', length: 36 })
+  readonly questionId: string
+
+  @Column({ type: 'text' })
+  readonly excerpt: string
+
+  @ManyToOne(() => User, user => user.answers)
+  @JoinColumn({ name: 'authorId' })
+  readonly author: User
+
+  @ManyToOne(() => Question, question => question.answers, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'questionId' })
+  readonly question: Question
+
+  @OneToMany(() => Comment, comment => comment.answer)
+  readonly comments: Comment[]
+
+  @OneToMany(() => Attachment, attachment => attachment.answer)
+  readonly attachments: Attachment[]
+
+  private constructor (props: AnswerProps, id?: string) {
+    super(id)
+    Object.assign(this, props)
+  }
+
+  static create (props: AnswerProps, id?: string): Answer {
+    const excerpt = props.excerpt ?? props.content.substring(0, 45).replace(/ $/, '').concat('...')
+    return new Answer({ ...props, excerpt }, id)
+  }
 }
