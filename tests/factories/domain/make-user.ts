e13f90f41ac4649eb4a1fa3a69db93bd08c560d8
manoel-lopes <manoel.lopes.shell@gmail.com>
2025-12-20 16:43:44 -0300

    test(factories): update make-user for entity classes

--- tests/factories/domain/make-user.ts
+++ tests/factories/domain/make-user.ts
@@ -1,12 +1,12 @@
-import type { UserProps } from '@/domain/enterprise/entities/user.entity'
+import { User, UserProps } from '@/domain/enterprise/entities/user.entity'
 import { faker } from '@faker-js/faker'
 
-export function makeUserData (override: Partial<UserProps> = {}): UserProps {
-  const user: UserProps = {
+export function makeUser (override: Partial<User> = {}): User {
+  const props: UserProps = {
     name: faker.person.fullName(),
     email: faker.internet.email(),
     password: faker.internet.password(),
     ...override,
   }
-  return user
+  return User.create(props)
 }
