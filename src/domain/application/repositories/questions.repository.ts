fb2b27de0307a3ec3f8d5449d8814b687b7aad11
manoel-lopes <manoel.lopes.shell@gmail.com>
2025-12-20 16:41:40 -0300

    refactor(repositories): update questions repository interface

--- src/domain/application/repositories/questions.repository.ts
+++ src/domain/application/repositories/questions.repository.ts
@@ -1,11 +1,7 @@
 import type { PaginatedItems } from '@/core/domain/application/paginated-items'
 import type { PaginationParams } from '@/core/domain/application/pagination-params'
-import type { Question, QuestionProps } from '@/domain/enterprise/entities/question.entity'
-import type { QuestionAttachment } from '@/domain/enterprise/entities/question-attachment.entity'
-import type { QuestionComment } from '@/domain/enterprise/entities/question-comment.entity'
-import type { User } from '@/domain/enterprise/entities/user.entity'
+import type { Question } from '@/domain/enterprise/entities/question.entity'
 import type { ForumIncludeOption } from '@/shared/types/forum/include-option'
-import type { PaginatedAnswers } from './answers.repository'
 
 export type PaginationWithIncludeParams = PaginationParams & {
   include?: ForumIncludeOption[]
@@ -22,14 +18,7 @@ export type FindQuestionBySlugParams = PaginationParams & {
   answerIncludes?: ForumIncludeOption[]
 }
 
-export type QuestionWithRelations = Question & {
-  answers?: PaginatedAnswers
-  comments?: QuestionComment[]
-  attachments?: QuestionAttachment[]
-  author?: Omit<User, 'password'>
-}
-
-export type PaginatedQuestions = Required<PaginatedItems<QuestionWithRelations>>
+export type PaginatedQuestions = Required<PaginatedItems<Question>>
 
 export type FindQuestionsResult = Question | null
 
@@ -38,7 +27,7 @@ export type FindManyQuestionsParams = PaginationParams & {
 }
 
 export type QuestionsRepository = {
-  create(question: QuestionProps): Promise<Question>
+  save(question: Question): Promise<void>
   findById(questionId: string): Promise<Question | null>
   findByTitle(questionTitle: string): Promise<Question | null>
   findBySlug(params: FindQuestionBySlugParams): Promise<FindQuestionsResult | null>
