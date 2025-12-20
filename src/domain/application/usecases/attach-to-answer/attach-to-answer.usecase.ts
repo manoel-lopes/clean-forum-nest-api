71c21de98aabbe29e75fcacc95d3a06e066c91b0
manoel-lopes <manoel.lopes.shell@gmail.com>
2025-12-20 16:43:33 -0300

    refactor(usecases): update attach usecases for entity classes

--- src/domain/application/usecases/attach-to-answer/attach-to-answer.usecase.ts
+++ src/domain/application/usecases/attach-to-answer/attach-to-answer.usecase.ts
@@ -2,10 +2,14 @@ import { Inject, Injectable } from '@nestjs/common'
 import { UseCase } from '@/core/domain/application/use-case'
 import { AnswerAttachmentsRepository } from '@/domain/application/repositories/answer-attachments.repository'
 import { AnswersRepository } from '@/domain/application/repositories/answers.repository'
-import type { AnswerAttachment, AnswerAttachmentProps } from '@/domain/enterprise/entities/answer-attachment.entity'
+import { AnswerAttachment } from '@/domain/enterprise/entities/answer-attachment.entity'
 import { ResourceNotFoundException } from '@/shared/application/exceptions/resource-not-found.exception'
 
-type AttachToAnswerRequest = AnswerAttachmentProps
+type AttachToAnswerRequest = {
+  answerId: string
+  title: string
+  url: string
+}
 
 @Injectable()
 export class AttachToAnswerUseCase implements UseCase {
@@ -14,13 +18,17 @@ export class AttachToAnswerUseCase implements UseCase {
     @Inject(AnswerAttachmentsRepository) private readonly answerAttachmentsRepository: AnswerAttachmentsRepository
   ) {}
 
-  async execute (request: AttachToAnswerRequest): Promise<AnswerAttachment> {
+  async execute (request: AttachToAnswerRequest): Promise<void> {
     const { answerId, title, url } = request
     const answer = await this.answersRepository.findById(answerId)
     if (!answer) {
       throw new ResourceNotFoundException('Answer')
     }
-    const attachment = await this.answerAttachmentsRepository.create({ answerId, title, url })
-    return attachment
+    const attachment = AnswerAttachment.create({
+      answerId,
+      title,
+      url,
+    })
+    await this.answerAttachmentsRepository.save(attachment)
   }
 }
--- src/domain/application/usecases/attach-to-question/attach-to-question.usecase.ts
+++ src/domain/application/usecases/attach-to-question/attach-to-question.usecase.ts
@@ -2,13 +2,14 @@ import { Inject, Injectable } from '@nestjs/common'
 import { UseCase } from '@/core/domain/application/use-case'
 import { QuestionAttachmentsRepository } from '@/domain/application/repositories/question-attachments.repository'
 import { QuestionsRepository } from '@/domain/application/repositories/questions.repository'
-import type {
-  QuestionAttachment,
-  QuestionAttachmentProps,
-} from '@/domain/enterprise/entities/question-attachment.entity'
+import { QuestionAttachment } from '@/domain/enterprise/entities/question-attachment.entity'
 import { ResourceNotFoundException } from '@/shared/application/exceptions/resource-not-found.exception'
 
-type AttachToQuestionRequest = QuestionAttachmentProps
+type AttachToQuestionRequest = {
+  questionId: string
+  title: string
+  url: string
+}
 
 @Injectable()
 export class AttachToQuestionUseCase implements UseCase {
@@ -17,13 +18,17 @@ export class AttachToQuestionUseCase implements UseCase {
     @Inject(QuestionAttachmentsRepository) private readonly questionAttachmentsRepository: QuestionAttachmentsRepository
   ) {}
 
-  async execute (request: AttachToQuestionRequest): Promise<QuestionAttachment> {
+  async execute (request: AttachToQuestionRequest): Promise<void> {
     const { questionId, title, url } = request
     const question = await this.questionsRepository.findById(questionId)
     if (!question) {
       throw new ResourceNotFoundException('Question')
     }
-    const attachment = await this.questionAttachmentsRepository.create({ questionId, title, url })
-    return attachment
+    const attachment = QuestionAttachment.create({
+      questionId,
+      title,
+      url,
+    })
+    await this.questionAttachmentsRepository.save(attachment)
   }
 }
