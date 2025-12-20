f1598b68c41459a03b64839e6f69ecc08a472c21
manoel-lopes <manoel.lopes.shell@gmail.com>
2025-12-20 16:41:15 -0300

    refactor(entities): convert attachment to typeorm entity class

--- src/domain/enterprise/entities/base/attachment.entity.ts
+++ src/domain/enterprise/entities/base/attachment.entity.ts
@@ -1,9 +1,42 @@
-import type { Entity } from '@/core/domain/entity'
-import type { Props } from '@/shared/types/custom/props'
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  TableInheritance,
+} from 'typeorm'
+import { Props } from '@/shared/types/custom/props'
+import { Answer } from '../answer.entity'
+import { Question } from '../question.entity'
+import { BaseEntity } from './base.entity'
 
 export type AttachmentProps = Props<Attachment>
 
-export interface Attachment extends Entity {
-  title: string
-  url: string
+@Entity('attachments')
+@TableInheritance({ column: { type: 'varchar', name: 'type' } })
+export abstract class Attachment extends BaseEntity {
+  @Column({ type: 'text' })
+  readonly title: string
+
+  @Column({ type: 'text' })
+  readonly url: string
+
+  @Column({ type: 'varchar', length: 36, nullable: true })
+  readonly questionId?: string
+
+  @Column({ type: 'varchar', length: 36, nullable: true })
+  readonly answerId?: string
+
+  @ManyToOne(() => Question, question => question.attachments, { onDelete: 'CASCADE', nullable: true })
+  @JoinColumn({ name: 'questionId' })
+  readonly question: Question
+
+  @ManyToOne(() => Answer, answer => answer.attachments, { onDelete: 'CASCADE', nullable: true })
+  @JoinColumn({ name: 'answerId' })
+  readonly answer: Answer
+
+  protected constructor (props: AttachmentProps, id?: string) {
+    super(id)
+    Object.assign(this, props)
+  }
 }
