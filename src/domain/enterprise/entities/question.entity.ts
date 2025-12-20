26cda44841de82a78117175cc7277a3621745972
manoel-lopes <manoel.lopes.shell@gmail.com>
2025-12-20 16:41:05 -0300

    refactor(entities): convert question to typeorm entity class

--- src/domain/enterprise/entities/question.entity.ts
+++ src/domain/enterprise/entities/question.entity.ts
@@ -1,13 +1,53 @@
-import type { Entity } from '@/core/domain/entity'
-import type { Optional } from '@/shared/types/common/optional'
-import type { Props } from '@/shared/types/custom/props'
+import { Column, Entity, JoinColumn, ManyToOne, OneToMany } from 'typeorm'
+import { Optional } from '@/shared/types/common/optional'
+import { Props } from '@/shared/types/custom/props'
+import { Slug } from '../value-objects/slug/slug.vo'
+import { Answer } from './answer.entity'
+import { Attachment } from './base/attachment.entity'
+import { BaseEntity } from './base/base.entity'
+import { Comment } from './base/comment.entity'
+import { User } from './user.entity'
 
-export type QuestionProps = Optional<Omit<Props<Question>, 'answers'>, 'bestAnswerId'>
+export type QuestionProps = Optional<Props<Question>, 'slug'>
 
-export interface Question extends Entity {
-  authorId: string
-  title: string
-  content: string
-  slug: string
-  bestAnswerId?: string | null
+@Entity('questions')
+export class Question extends BaseEntity {
+  @Column({ type: 'text' })
+  readonly title: string
+
+  @Column({ type: 'text', unique: true })
+  readonly slug: string
+
+  @Column({ type: 'text' })
+  readonly content: string
+
+  @Column({ type: 'varchar', length: 36 })
+  readonly authorId: string
+
+  @Column({ type: 'varchar', length: 36, nullable: true })
+  readonly bestAnswerId?: string
+
+  @ManyToOne(() => User, user => user.questions)
+  @JoinColumn({ name: 'authorId' })
+  readonly author: User
+
+  @OneToMany(() => Answer, answer => answer.question)
+  readonly answers: Answer[]
+
+  @OneToMany(() => Comment, comment => comment.question)
+  readonly comments: Comment[]
+
+  @OneToMany(() => Attachment, attachment => attachment.question)
+  readonly attachments: Attachment[]
+
+  private constructor (props: QuestionProps, id?: string) {
+    super(id)
+    Object.assign(this, props)
+  }
+
+  static create (props: QuestionProps, id?: string): Question {
+    const slug = Slug.create(props.title)
+    const questionSlug = props.slug ?? slug.value
+    return new Question({ ...props, slug: questionSlug }, id)
+  }
 }
