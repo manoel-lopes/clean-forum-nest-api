b88bb73d90ebbbc0673cd7cb44a6ab4944accced
manoel-lopes <manoel.lopes.shell@gmail.com>
2025-12-20 16:41:16 -0300

    refactor(entities): convert email-validation to typeorm entity class

--- src/domain/enterprise/entities/email-validation.entity.ts
+++ src/domain/enterprise/entities/email-validation.entity.ts
@@ -1,11 +1,32 @@
-import type { Entity } from '@/core/domain/entity'
-import type { Props } from '@/shared/types/custom/props'
+import { Column, Entity } from 'typeorm'
+import { Props } from '@/shared/types/custom/props'
+import { EmailValidationCode } from '../value-objects/email-validation-code/email-validation-code.vo'
+import { BaseEntity } from './base/base.entity'
 
-export type EmailValidationProps = Props<EmailValidation>
+export type EmailValidationProps = Omit<Props<EmailValidation>, 'code'>
 
-export interface EmailValidation extends Entity {
-  email: string
-  code: string
-  expiresAt: Date
-  isVerified: boolean
+@Entity('email_validations')
+export class EmailValidation extends BaseEntity {
+  @Column({ type: 'text', unique: true })
+  readonly email: string
+
+  @Column({ type: 'text' })
+  readonly code: string
+
+  @Column({ type: 'timestamptz' })
+  readonly expiresAt: Date
+
+  @Column({ type: 'boolean', default: false })
+  readonly isVerified: boolean
+
+  private constructor (props: Omit<EmailValidation, 'id'>, id?: string) {
+    super(id)
+    Object.assign(this, props)
+  }
+
+  static create (props: EmailValidationProps, id?: string): EmailValidation {
+    const validationCode = EmailValidationCode.create()
+    const code = validationCode.value
+    return new EmailValidation({ ...props, code }, id)
+  }
 }
