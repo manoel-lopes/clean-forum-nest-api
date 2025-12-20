a09a55774d62e25c0bd89e6f581defba5188693a
manoel-lopes <manoel.lopes.shell@gmail.com>
2025-12-20 16:41:31 -0300

    refactor(entities): convert answer-attachment to typeorm entity class

--- src/domain/enterprise/entities/answer-attachment.entity.ts
+++ src/domain/enterprise/entities/answer-attachment.entity.ts
@@ -1,8 +1,16 @@
-import type { Props } from '@/shared/types/custom/props'
-import type { Attachment } from './base/attachment.entity'
+import { ChildEntity } from 'typeorm'
+import { Props } from '@/shared/types/custom/props'
+import { Attachment } from './base/attachment.entity'
 
 export type AnswerAttachmentProps = Props<AnswerAttachment>
 
-export interface AnswerAttachment extends Attachment {
-  answerId: string
+@ChildEntity('answer')
+export class AnswerAttachment extends Attachment {
+  private constructor (props: AnswerAttachmentProps, id?: string) {
+    super(props, id)
+  }
+
+  static create (props: AnswerAttachmentProps, id?: string): AnswerAttachment {
+    return new AnswerAttachment(props, id)
+  }
 }
