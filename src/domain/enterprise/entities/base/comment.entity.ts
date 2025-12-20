54a5eab897523b60c1dbbe3d9d56c978b2bb3669
manoel-lopes <manoel.lopes.shell@gmail.com>
2025-12-20 16:41:15 -0300

    refactor(entities): convert comment to typeorm entity class

--- src/domain/enterprise/entities/base/comment.entity.ts
+++ src/domain/enterprise/entities/base/comment.entity.ts
@@ -1,9 +1,41 @@
-import type { Entity } from '@/core/domain/entity'
-import type { Props } from '@/shared/types/custom/props'
+import { Column, Entity, JoinColumn, ManyToOne, TableInheritance } from 'typeorm'
+import { Props } from '@/shared/types/custom/props'
+import { Answer } from '../answer.entity'
+import { Question } from '../question.entity'
+import { User } from '../user.entity'
+import { BaseEntity } from './base.entity'
 
 export type CommentProps = Props<Comment>
 
-export interface Comment extends Entity {
-  authorId: string
-  content: string
+@Entity('comments')
+@TableInheritance({ column: { type: 'varchar', name: 'type' } })
+export abstract class Comment extends BaseEntity {
+  @Column({ type: 'text' })
+  readonly content: string
+
+  @Column({ type: 'varchar', length: 36 })
+  readonly authorId: string
+
+  @Column({ type: 'varchar', length: 36, nullable: true })
+  readonly questionId?: string
+
+  @Column({ type: 'varchar', length: 36, nullable: true })
+  readonly answerId?: string
+
+  @ManyToOne(() => User, user => user.comments)
+  @JoinColumn({ name: 'authorId' })
+  readonly author: User
+
+  @ManyToOne(() => Question, question => question.comments, { onDelete: 'CASCADE', nullable: true })
+  @JoinColumn({ name: 'questionId' })
+  readonly question?: Question
+
+  @ManyToOne(() => Answer, answer => answer.comments, { onDelete: 'CASCADE', nullable: true })
+  @JoinColumn({ name: 'answerId' })
+  readonly answer?: Answer
+
+  protected constructor (props: CommentProps, id?: string) {
+    super(id)
+    Object.assign(this, props)
+  }
 }
