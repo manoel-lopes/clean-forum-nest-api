0b52d78f12a5d7b0a8672fd244c3f942acafd0c2
manoel-lopes <manoel.lopes.shell@gmail.com>
2025-12-20 16:42:49 -0300

    refactor(in-memory): update comments repository

--- src/infra/persistence/repositories/in-memory/in-memory-comments.repository.ts
+++ src/infra/persistence/repositories/in-memory/in-memory-comments.repository.ts
@@ -1,11 +1,11 @@
 import type { CommentsRepository, UpdateCommentData } from '@/domain/application/repositories/base/comments.repository'
-import type { Comment } from '@/domain/enterprise/entities/base/comment.entity'
+import { Comment } from '@/domain/enterprise/entities/base/comment.entity'
 import { BaseInMemoryRepository as BaseRepository } from './base/base-in-memory.repository'
 
-export class InMemoryCommentsRepository<T extends Comment = Comment>
-  extends BaseRepository<T>
-  implements CommentsRepository<T> {
-  async update (commentData: UpdateCommentData): Promise<T> {
+export class InMemoryCommentsRepository
+  extends BaseRepository<Comment>
+  implements CommentsRepository<Comment> {
+  async update (commentData: UpdateCommentData): Promise<Comment> {
     const { where, data } = commentData
     const updatedComment = await this.updateOne({ where, data })
     return updatedComment
