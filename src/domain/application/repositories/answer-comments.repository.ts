9a938e0f661cbce1caf7ef16faf76d7e3ef1070f
manoel-lopes <manoel.lopes.shell@gmail.com>
2025-12-20 16:41:48 -0300

    refactor(repositories): update comment repositories interfaces

--- src/domain/application/repositories/answer-comments.repository.ts
+++ src/domain/application/repositories/answer-comments.repository.ts
@@ -1,12 +1,12 @@
 import type { PaginatedItems } from '@/core/domain/application/paginated-items'
 import type { PaginationParams } from '@/core/domain/application/pagination-params'
-import type { AnswerComment, AnswerCommentProps } from '@/domain/enterprise/entities/answer-comment.entity'
+import type { Comment } from '@/domain/enterprise/entities/base/comment.entity'
 import type { CommentsRepository } from './base/comments.repository'
 
-export type PaginatedAnswerComments = Required<PaginatedItems<AnswerComment>>
+export type PaginatedAnswerComments = Required<PaginatedItems<Comment>>
 
-export type AnswerCommentsRepository = CommentsRepository<AnswerComment> & {
-  create(comment: AnswerCommentProps): Promise<AnswerComment>
+export type AnswerCommentsRepository = CommentsRepository<Comment> & {
+  save(comment: Comment): Promise<void>
   findManyByAnswerId(answerId: string, params: PaginationParams): Promise<PaginatedAnswerComments>
 }
 
--- src/domain/application/repositories/question-comments.repository.ts
+++ src/domain/application/repositories/question-comments.repository.ts
@@ -1,12 +1,12 @@
 import type { PaginatedItems } from '@/core/domain/application/paginated-items'
 import type { PaginationParams } from '@/core/domain/application/pagination-params'
-import type { QuestionComment, QuestionCommentProps } from '@/domain/enterprise/entities/question-comment.entity'
+import type { Comment } from '@/domain/enterprise/entities/base/comment.entity'
 import type { CommentsRepository } from './base/comments.repository'
 
-export type PaginatedQuestionComments = Required<PaginatedItems<QuestionComment>>
+export type PaginatedQuestionComments = Required<PaginatedItems<Comment>>
 
-export type QuestionCommentsRepository = CommentsRepository<QuestionComment> & {
-  create(comment: QuestionCommentProps): Promise<QuestionComment>
+export type QuestionCommentsRepository = CommentsRepository<Comment> & {
+  save(comment: Comment): Promise<void>
   findManyByQuestionId(questionId: string, params: PaginationParams): Promise<PaginatedQuestionComments>
 }
 
