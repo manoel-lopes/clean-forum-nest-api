717c40700e496adc5e30f438fa3dacd415e0c09e
manoel-lopes <manoel.lopes.shell@gmail.com>
2025-12-20 16:41:04 -0300

    refactor(entities): convert user to typeorm entity class

--- src/domain/enterprise/entities/user.entity.ts
+++ src/domain/enterprise/entities/user.entity.ts
@@ -1,10 +1,42 @@
-import type { Entity } from '@/core/domain/entity'
-import type { Props } from '@/shared/types/custom/props'
+import { Column, Entity, OneToMany } from 'typeorm'
+import { Props } from '@/shared/types/custom/props'
+import { Answer } from './answer.entity'
+import { BaseEntity } from './base/base.entity'
+import { Comment } from './base/comment.entity'
+import { Question } from './question.entity'
+import { RefreshToken } from './refresh-token.entity'
 
 export type UserProps = Props<User>
 
-export interface User extends Entity {
-  name: string
-  email: string
-  password: string
+@Entity('users')
+export class User extends BaseEntity {
+  @Column({ type: 'text' })
+  readonly name: string
+
+  @Column({ type: 'text', unique: true })
+  readonly email: string
+
+  @Column({ type: 'text' })
+  readonly password: string
+
+  @OneToMany(() => Question, question => question.author)
+  readonly questions: Question[]
+
+  @OneToMany(() => Answer, answer => answer.author)
+  readonly answers: Answer[]
+
+  @OneToMany(() => Comment, comment => comment.author)
+  readonly comments: Comment[]
+
+  @OneToMany(() => RefreshToken, refreshToken => refreshToken.user)
+  readonly refreshTokens: RefreshToken[]
+
+  private constructor (props: UserProps, id?: string) {
+    super(id)
+    Object.assign(this, props)
+  }
+
+  static create (props: UserProps, id?: string): User {
+    return new User(props, id)
+  }
 }
