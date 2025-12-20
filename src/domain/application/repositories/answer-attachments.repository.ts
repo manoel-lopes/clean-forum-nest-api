c2429640d4e9a0590e64f004fd73dee69be0b2c4
manoel-lopes <manoel.lopes.shell@gmail.com>
2025-12-20 16:41:49 -0300

    refactor(repositories): update attachment repositories interfaces

--- src/domain/application/repositories/answer-attachments.repository.ts
+++ src/domain/application/repositories/answer-attachments.repository.ts
@@ -1,15 +1,15 @@
 import type { PaginatedItems } from '@/core/domain/application/paginated-items'
 import type { PaginationParams } from '@/core/domain/application/pagination-params'
-import type { AnswerAttachment, AnswerAttachmentProps } from '@/domain/enterprise/entities/answer-attachment.entity'
+import type { Attachment } from '@/domain/enterprise/entities/base/attachment.entity'
 
-export type PaginatedAnswerAttachments = Required<PaginatedItems<AnswerAttachment>>
+export type PaginatedAnswerAttachments = Required<PaginatedItems<Attachment>>
 
 export type AnswerAttachmentsRepository = {
-  create(attachment: AnswerAttachmentProps): Promise<AnswerAttachment>
-  createMany(attachments: AnswerAttachmentProps[]): Promise<AnswerAttachment[]>
-  findById(attachmentId: string): Promise<AnswerAttachment | null>
+  save(attachment: Attachment): Promise<void>
+  saveMany(attachments: Attachment[]): Promise<void>
+  findById(attachmentId: string): Promise<Attachment | null>
   findManyByAnswerId(answerId: string, params: PaginationParams): Promise<PaginatedAnswerAttachments>
-  update(attachmentId: string, data: Partial<Pick<AnswerAttachment, 'title' | 'url'>>): Promise<AnswerAttachment>
+  update(attachmentId: string, data: Partial<Pick<Attachment, 'title' | 'url'>>): Promise<Attachment>
   delete(attachmentId: string): Promise<void>
   deleteMany(attachmentIds: string[]): Promise<void>
 }
--- src/domain/application/repositories/question-attachments.repository.ts
+++ src/domain/application/repositories/question-attachments.repository.ts
@@ -1,18 +1,15 @@
 import type { PaginatedItems } from '@/core/domain/application/paginated-items'
 import type { PaginationParams } from '@/core/domain/application/pagination-params'
-import type {
-  QuestionAttachment,
-  QuestionAttachmentProps,
-} from '@/domain/enterprise/entities/question-attachment.entity'
+import type { Attachment } from '@/domain/enterprise/entities/base/attachment.entity'
 
-export type PaginatedQuestionAttachments = Required<PaginatedItems<QuestionAttachment>>
+export type PaginatedQuestionAttachments = Required<PaginatedItems<Attachment>>
 
 export type QuestionAttachmentsRepository = {
-  create(attachment: QuestionAttachmentProps): Promise<QuestionAttachment>
-  createMany(attachments: QuestionAttachmentProps[]): Promise<QuestionAttachment[]>
-  findById(attachmentId: string): Promise<QuestionAttachment | null>
+  save(attachment: Attachment): Promise<void>
+  saveMany(attachments: Attachment[]): Promise<void>
+  findById(attachmentId: string): Promise<Attachment | null>
   findManyByQuestionId(questionId: string, params: PaginationParams): Promise<PaginatedQuestionAttachments>
-  update(attachmentId: string, data: Partial<Pick<QuestionAttachment, 'title' | 'url'>>): Promise<QuestionAttachment>
+  update(attachmentId: string, data: Partial<Pick<Attachment, 'title' | 'url'>>): Promise<Attachment>
   delete(attachmentId: string): Promise<void>
   deleteMany(attachmentIds: string[]): Promise<void>
 }
