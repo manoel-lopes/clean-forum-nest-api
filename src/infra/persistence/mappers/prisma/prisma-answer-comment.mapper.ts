5659b2b33b0ee3e9afc53be10bbf1b1085c7d1e1
manoel-lopes <manoel.lopes.shell@gmail.com>
2025-12-20 16:39:51 -0300

    refactor: remove remaining prisma mappers

--- src/infra/persistence/mappers/prisma/prisma-answer-comment.mapper.ts
+++
@@ -1,9 +0,0 @@
-import type { Comment } from '@prisma/client'
-import type { AnswerComment } from '@/domain/enterprise/entities/answer-comment.entity'
-import { BasePrismaMapper } from './base/base-prisma.mapper'
-
-export class PrismaAnswerCommentMapper {
-  static toDomain (raw: Comment): AnswerComment {
-    return BasePrismaMapper.mapAnswerComment(raw)
-  }
-}
