463dec8e2ed93cc035c05901ea57948e6eaf75b2
manoel-lopes <manoel.lopes.shell@gmail.com>
2025-12-21 15:47:11 -0300

    refactor(repositories): delete typeorm mappers

--- src/infra/persistence/mappers/typeorm/typeorm-email-validation.mapper.ts
+++
@@ -1,7 +0,0 @@
-import { EmailValidation } from '@/domain/enterprise/entities/email-validation.entity'
-
-export class TypeOrmEmailValidationMapper {
-  static toDomain (raw: EmailValidation): EmailValidation {
-    return raw
-  }
-}
