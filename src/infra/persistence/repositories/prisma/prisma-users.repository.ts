031226f909ed2b0e525eb7271277a5cb38a228e8
manoel-lopes <manoel.lopes.shell@gmail.com>
2025-12-22 14:05:38 -0300

    refactor(users): use formatPagination helper

--- src/infra/persistence/repositories/prisma/prisma-users.repository.ts
+++ src/infra/persistence/repositories/prisma/prisma-users.repository.ts
@@ -2,18 +2,14 @@ import { Injectable } from '@nestjs/common'
 import type { PaginatedItems } from '@/core/domain/application/paginated-items'
 import type { PaginationParams } from '@/core/domain/application/pagination-params'
 import type { UpdateUserData, UsersRepository, UserWithoutPassword } from '@/domain/application/repositories/users.repository'
+import { formatPagination } from '@/infra/persistence/helpers/format-pagination.helper'
 import { PrismaUserMapper } from '@/infra/persistence/mappers/prisma/prisma-user.mapper'
 import { PrismaService } from '@/infra/persistence/prisma.service'
 import type { User, UserProps } from '@/domain/enterprise/entities/user.entity'
-import { BasePrismaRepository } from './base/base-prisma.repository'
 
 @Injectable()
-export class PrismaUsersRepository
-  extends BasePrismaRepository
-  implements UsersRepository {
-  constructor (private readonly prisma: PrismaService) {
-    super()
-  }
+export class PrismaUsersRepository implements UsersRepository {
+  constructor (private readonly prisma: PrismaService) {}
 
   async create (data: UserProps): Promise<UserWithoutPassword> {
     const user = await this.prisma.user.create({ data })
@@ -48,7 +44,7 @@ export class PrismaUsersRepository
   }
 
   async findMany ({ page = 1, pageSize = 10, order = 'desc' }: PaginationParams): Promise<PaginatedItems<UserWithoutPassword>> {
-    const pagination = this.sanitizePagination(page, pageSize)
+    const pagination = formatPagination(page, pageSize)
     const [users, totalItems] = await this.prisma.$transaction([
       this.prisma.user.findMany({
         skip: pagination.skip,
