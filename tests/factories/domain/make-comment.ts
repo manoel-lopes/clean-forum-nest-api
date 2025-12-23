0312fbaeb8ad995624b88644ae5c0a0141fe8215
manoel-lopes <manoel.lopes.shell@gmail.com>
2025-12-22 22:09:46 -0300

    test(factories): exclude type from base comment factory

--- tests/factories/domain/make-comment.ts
+++ tests/factories/domain/make-comment.ts
@@ -1,8 +1,10 @@
 import type { Comment, CommentProps } from '@/domain/enterprise/entities/base/comment.entity'
 import { faker } from '@faker-js/faker'
 
-export function makeCommentData (override: Partial<Comment> = {}): CommentProps {
-  const comment: CommentProps = {
+type BaseCommentData = Omit<CommentProps, 'type'>
+
+export function makeCommentData (override: Partial<Comment> = {}): BaseCommentData {
+  const comment: BaseCommentData = {
     content: faker.lorem.sentence(),
     authorId: faker.string.uuid(),
   }
