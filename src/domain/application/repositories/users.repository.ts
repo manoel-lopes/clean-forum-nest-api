df76a5d9665729bee459a899f6a3fa1a447e4168
manoel-lopes <manoel.lopes.shell@gmail.com>
2025-12-23 22:56:31 -0300

    refactor(users): update repository interface to return User with password

--- src/domain/application/repositories/users.repository.ts
+++ src/domain/application/repositories/users.repository.ts
@@ -2,19 +2,17 @@ import type { PaginatedItems } from '@/core/domain/application/paginated-items'
 import type { PaginationParams } from '@/core/domain/application/pagination-params'
 import type { User, UserProps } from '@/domain/enterprise/entities/user.entity'
 
-export type UserWithoutPassword = Omit<User, 'password'>
-
 export type UpdateUserData = {
   where: { id: string }
   data: Partial<Omit<User, 'id' | 'createdAt' | 'updatedAt'>>
 }
 
-export type PaginatedUsers = Required<PaginatedItems<UserWithoutPassword>>
+export type PaginatedUsers = Required<PaginatedItems<User>>
 
 export type UsersRepository = {
-  create(user: UserProps): Promise<UserWithoutPassword>
+  create(user: UserProps): Promise<User>
   update(user: UpdateUserData): Promise<User>
-  findById(userId: string): Promise<UserWithoutPassword | null>
+  findById(userId: string): Promise<User | null>
   delete(userId: string): Promise<void>
   findByEmail(email: string): Promise<User | null>
   findMany(paginationParams: PaginationParams): Promise<PaginatedUsers>
