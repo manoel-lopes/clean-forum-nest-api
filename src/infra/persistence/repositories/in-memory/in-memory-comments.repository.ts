b25a63428d9414e935b825b361c57620e9d648f9
manoel-lopes <manoel.lopes.shell@gmail.com>
2025-12-30 13:55:55 -0300

    refactor(in-memory): consolidate comment repositories

--- src/infra/persistence/repositories/in-memory/in-memory-comments.repository.ts
+++ src/infra/persistence/repositories/in-memory/in-memory-comments.repository.ts
@@ -1,12 +1,31 @@
-import type { CommentsRepository, UpdateCommentData } from '@/domain/application/repositories/base/comments.repository'
-import { Comment } from '@/domain/enterprise/entities/base/comment.entity'
+import type { PaginationParams } from '@/core/domain/pagination-params'
+import type {
+  CommentsRepository,
+  PaginatedComments,
+  UpdateCommentData,
+} from '@/domain/application/repositories/comments.repository'
+import { Comment, CommentProps } from '@/domain/enterprise/entities/comment.entity'
 import { BaseInMemoryRepository as BaseRepository } from './base/base-in-memory.repository'
 
 export class InMemoryCommentsRepository
   extends BaseRepository<Comment>
-  implements CommentsRepository<Comment> {
-  async update ({ commentId, data }: UpdateCommentData): Promise<Comment> {
-    const updatedComment = await this.updateOne({ id: commentId, ...data })
+  implements CommentsRepository {
+  async create (data: CommentProps): Promise<Comment> {
+    const comment = Comment.create(data)
+    await this.save(comment)
+    return comment
+  }
+
+  async findManyByAnswerId (answerId: string, params: PaginationParams): Promise<PaginatedComments> {
+    const result = await this.findManyItemsBy({
+      where: { answerId } as Partial<Comment>,
+      params,
+    })
+    return result as PaginatedComments
+  }
+
+  async update ({ where, data }: UpdateCommentData): Promise<Comment> {
+    const updatedComment = await this.updateOne({ id: where.id, ...data })
     return updatedComment
   }
 }
