40080ed706a6102b1c7813aca42d44c3b2573995
manoel-lopes <manoel.lopes.shell@gmail.com>
2025-12-31 17:07:12 -0300

    build(prisma): split schema into multiple files

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,7 +3,7 @@ import { defineConfig, env } from "prisma/config";
 const databaseUrl = `postgresql://${env("DB_USER")}:${env("DB_PASSWORD")}@${env("DB_HOST")}:${env("DB_PORT")}/${env("DB_NAME")}?schema=public`;
 
 export default defineConfig({
-  schema: "prisma/schema.prisma",
+  schema: "prisma",
   migrations: {
     path: "prisma/migrations",
     seed: "npx tsx prisma/seed.ts",
