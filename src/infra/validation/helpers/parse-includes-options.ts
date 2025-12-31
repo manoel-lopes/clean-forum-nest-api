beddc8d7a0b7a3e73326acd1b97adfa602a8829a
manoel-lopes <manoel.lopes.shell@gmail.com>
2025-12-31 17:08:21 -0300

    refactor(validation): rename parse-include-options to parse-includes-options

--- src/infra/validation/helpers/parse-includes-options.ts
+++ src/infra/validation/helpers/parse-includes-options.ts
@@ -2,13 +2,13 @@ import type { ForumIncludeOptions } from '@/shared/types/forum/include-option'
 
 type IncludeKey = keyof ForumIncludeOptions
 
-export function parseIncludeOptions (value?: string | null): ForumIncludeOptions {
-  const isValidIncludeKey = (item: string): item is IncludeKey => {
-    return ['comments', 'attachments', 'author'].includes(item)
-  }
+export function parseIncludesOptions (
+  value?: string | null,
+  includes: string[] = []
+): ForumIncludeOptions {
+  const isValidIncludeKey = (item: string): item is IncludeKey => includes.includes(item)
   if (!value) return {}
   return value.split(',')
-    .map(s => s.trim())
     .reduce((acc: ForumIncludeOptions, item) => ({
       ...acc,
       [item]: isValidIncludeKey(item),
