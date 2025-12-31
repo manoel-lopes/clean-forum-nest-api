80e8dfa04401c199b6e3534e5a50c197c9819dfa
manoel-lopes <manoel.lopes.shell@gmail.com>
2025-12-31 17:09:03 -0300

    refactor(validation): update fetch-questions schema to use new helper

--- src/infra/validation/schemas/presentation/questions/fetch-questions.schema.ts
+++ src/infra/validation/schemas/presentation/questions/fetch-questions.schema.ts
@@ -1,10 +1,12 @@
 import { createZodDto } from 'nestjs-zod'
 import { z } from 'zod'
-import { parseIncludeOptions } from '@/infra/validation/helpers/parse-include-options'
+import { parseIncludesOptions } from '@/infra/validation/helpers/parse-includes-options'
 import { paginationQuerySchema } from '@/infra/validation/schemas/core/pagination.schema'
 
+const includes = ['attachments', 'author']
+
 export const fetchQuestionsQuerySchema = paginationQuerySchema.extend({
-  include: z.string().optional().transform(parseIncludeOptions),
+  include: z.string().optional().transform(value => parseIncludesOptions(value, includes)),
 })
 
 export type FetchQuestionsQuery = z.infer<typeof fetchQuestionsQuerySchema>
