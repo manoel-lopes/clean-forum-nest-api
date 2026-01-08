a0e39c15aa37a2bbc63ef96763cce39f83064f3f
manoel-lopes <manoel.lopes.shell@gmail.com>
2026-01-08 11:38:29 -0300

    refactor(decorator): update uploaded-file import for new interceptor location

--- src/infra/http/presentation/decorators/uploaded-file.decorator.ts
+++ src/infra/http/presentation/decorators/uploaded-file.decorator.ts
@@ -1,10 +1,14 @@
 import type { FastifyRequest } from 'fastify'
 import { createParamDecorator, ExecutionContext } from '@nestjs/common'
-import { UPLOADED_FILE_KEY, UploadedFileData } from '@/infra/http/interceptors/fastify-file.interceptor'
+import {
+  UPLOADED_FILE_KEY,
+  UploadedFileData
+} from '@/infra/http/presentation/interceptors/fastify-file.interceptor'
 
 export const UploadedFile = createParamDecorator(
   (_data: unknown, ctx: ExecutionContext): UploadedFileData => {
-    const request = ctx.switchToHttp().getRequest<FastifyRequest & { [UPLOADED_FILE_KEY]: UploadedFileData }>()
+    const request = ctx.switchToHttp().getRequest<FastifyRequest
+    & { [UPLOADED_FILE_KEY]: UploadedFileData }>()
     return request[UPLOADED_FILE_KEY]
   }
 )
